1b6c243e57d269129f88838a656c6f103f25ae54
Shresa <shreshthasadhu@gmail.com>
2021-08-06 16:44:28 +0530

    add statics,methods and refactor signup route

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
+// Importing Env Variables
+require("dotenv").config();
+
+// Libraries
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+
+// microservice routes
+import Auth from "./API/Auth";
+
+// Database connection
+import ConnectDB from "./database/connection";
 
 const foodstar = express();
 
@@ -10,6 +20,15 @@ foodstar.use(express.urlencoded({ extended: false}));
 foodstar.use(helmet());
 foodstar.use(cors());
 
+// Application Routes
+foodstar.use("/auth", Auth);
+
+
 foodstar.get("/", (req, res) => res.json({message: "Setup success"}));
 
-foodstar.listen(4000, () => console.log("Server is running"));
+foodstar.listen(4000, () =>  ConnectDB()
+    .then(() => console.log("Server is running"))
+    .catch(() =>
+    console.log("Server is running, but database connection failed... ")
+    )
+);
