d4fd78df49e69c727eb05e6d973e6265138af670
Shresa <shreshthasadhu@gmail.com>
2021-09-09 18:56:54 +0530

    add review reducers

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -48,6 +48,7 @@ Method    POST
 Router.post("/signin", async (req, res) => {
   try {
     await ValidateSignin(req.body.credentials);
+    
     const user = await UserModel.findByEmailAndPassword(req.body.credentials);
 
     const token = user.generateJwtToken();
